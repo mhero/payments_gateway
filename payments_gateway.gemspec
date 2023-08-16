b1274d587c0fe36583e047305d40ed9d0d604a58
Marco <gdmarav374@gmail.com>
2023-08-16 16:10:46 -0500

    Add factorybot to engine

--- payments_gateway.gemspec
+++ payments_gateway.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |spec|
   spec.metadata["changelog_uri"] = "https://github.com/mhero/payments-gateway"
 
   spec.add_development_dependency "rspec-rails"
+  spec.add_development_dependency "factory_bot_rails"
 
   spec.files = Dir.chdir(File.expand_path(__dir__)) do
     Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,18 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
+ENGINE_ROOT = File.join(File.dirname(__FILE__), '../')
+
 require_relative '../spec/dummy/config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+
+require 'factory_bot_rails'
+
+FactoryBot.definition_file_paths << File.join(File.dirname(__FILE__), 'factories')
+FactoryBot.factories.clear
+FactoryBot.find_definitions
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -26,6 +34,7 @@ require 'rspec/rails'
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
 begin
+  ActiveRecord::Migrator.migrations_paths = File.join(ENGINE_ROOT, 'spec/dummy/db/migrate')
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
@@ -61,4 +70,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.include FactoryBot::Syntax::Methods
 end
