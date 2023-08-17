3c170e060af776d6ab0ac6eb10f33e20876791fb
Marco <gdmarav374@gmail.com>
2023-08-17 04:43:47 -0500

    Use meta programming to define provider check

--- app/service/provider_service.rb
+++ app/service/provider_service.rb
@@ -3,17 +3,22 @@
 class ProviderService < ApplicationService
   attr_reader :provider
 
+  PROVIDERS = ['stripe'].freeze
+
+  PROVIDERS.each do |attribute|
+    define_singleton_method(:"#{attribute}?") do |attr_|
+      attribute == attr_
+    end
+  end
+
   def initialize(provider)
     @provider = provider
   end
 
   def call
+    # This method can support more providers by mapping them here
     return unless ProviderService.stripe?(@provider)
 
     StripeService
-  end
-
-  def self.stripe?(provider)
-    provider == 'stripe'
   end
 end
